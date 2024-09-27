1e187e3d0fd2f90df0e3d62349bc582f18f0abbf
kruceo <kruceo.com@gmail.com>
2024-09-27 11:48:53 -0300

    fix: improve install script

--- installLinux.sh
+++ installLinux.sh
@@ -41,25 +41,28 @@ cd "$INSTALL_PATH"
 
 # bash -c "sudo NODE_ENV=production $RUNTIME $ARGS /opt/webtouch/server/index.ts"
 
-echo [Unit]                                             >  /tmp/webtouch.service
-echo "Description=Webtouch Service"                     >> /tmp/webtouch.service
-echo "After=network.target"                             >> /tmp/webtouch.service
-echo ""                                                 >> /tmp/webtouch.service
-echo [Service]                                          >> /tmp/webtouch.service
+echo [Unit]                                                   >  /tmp/webtouch.service
+echo "Description=Webtouch Service"                           >> /tmp/webtouch.service
+echo "After=network.target"                                   >> /tmp/webtouch.service
+echo ""                                                       >> /tmp/webtouch.service
+echo [Service]                                                >> /tmp/webtouch.service
 echo "ExecStart=$RUNTIME $ARGS $INSTALL_PATH/server/index.ts" >> /tmp/webtouch.service
-echo "WorkingDirectory=$INSTALL_PATH"                   >> /tmp/webtouch.service
-echo "Restart=always"                                   >> /tmp/webtouch.service
-echo "RestartSec=10"                                    >> /tmp/webtouch.service
-echo "User=$(whoami)"                                   >> /tmp/webtouch.service
-echo "Environment=PORT=3000 NODE_ENV=production"        >> /tmp/webtouch.service
-echo "StandardOutput=syslog"                            >> /tmp/webtouch.service
-echo "StandardError=syslog"                             >> /tmp/webtouch.service
-echo ""                                                 >> /tmp/webtouch.service
-echo [Install]                                          >> /tmp/webtouch.service
-echo WantedBy=multi-user.target                         >> /tmp/webtouch.service
+echo "WorkingDirectory=$INSTALL_PATH"                         >> /tmp/webtouch.service
+echo "Restart=always"                                         >> /tmp/webtouch.service
+echo "RestartSec=10"                                          >> /tmp/webtouch.service
+echo "User=$(whoami)"                                         >> /tmp/webtouch.service
+echo "Environment=PORT=3000 NODE_ENV=production"              >> /tmp/webtouch.service
+echo "StandardOutput=syslog"                                  >> /tmp/webtouch.service
+echo "StandardError=syslog"                                   >> /tmp/webtouch.service
+echo ""                                                       >> /tmp/webtouch.service
+echo [Install]                                                >> /tmp/webtouch.service
+echo WantedBy=multi-user.target                               >> /tmp/webtouch.service
 
 mv /tmp/webtouch.service "$INSTALL_PATH/webtouch.service"
 
 sudo rm  /etc/systemd/system/webtouch.service
 sudo ln -s "$INSTALL_PATH/webtouch.service" "/etc/systemd/system/webtouch.service"
 
+sudo systemctl daemon-reload
+sudo systemctl enable webtouch.service
+sudo systemctl start webtouch.service
